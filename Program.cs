5fbdc9d30bd428b168193f5314220aea0782c499
jamjamstone <junghyun4545@naver.com>
2024-12-19 17:40:28 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace weekproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
